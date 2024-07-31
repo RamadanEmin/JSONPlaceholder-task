67d53eb2162e3d4437e15e246789c425453817e0
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-07-31 12:17:11 +0300

    Added User page and fetch users data

--- src/pages/UserPages.tsx
+++ src/pages/UserPages.tsx
@@ -1,9 +1,128 @@
-import React from 'react';
+import {
+    Box,
+    Button,
+    Container
+} from '@mui/material'
+import { useAppDispatch, useAppSelector } from '@/stores/Hooks';
+import { getUsers, deleteUser, selectUsers, UserProps } from '@/stores/features/users/userReducers';
+import { useEffect, useMemo, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const UserPages = () => {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+export default function UserPages() {
+    const dispatch = useAppDispatch();
+    const { users, pending } = useAppSelector(selectUsers);
+
+    const filters = useMemo(() => {
+        const params = {}
+        return params
+    }, []);
+
+    useEffect(() => {
+        dispatch(getUsers(filters))
+    }, [filters])
+
+    const UserData = useMemo(() => {
+        const result: UserProps[] = []
+        if (users?.length > 0) {
+            users?.map((user) => {
+                result.push({
+                    ...user,
+                })
+            })
+        }
+        return result
+    }, [users])
+
+    const [isOpenAlert, setIsOpenAlert] = useState<boolean>(false);
+    const [isMessage, setIsMessage] = useState<string>("");
+
+    const onDeleteUser = (id?: number | string) => {
+        if (!id) return;
+        dispatch(deleteUser({
+            id,
+            isSuccess: async () => {
+                setIsOpenAlert(true);
+                setIsMessage("User has been deleted.");
+            },
+            isError: async (error: any) => {
+                console.log(error)
+            }
+        }))
+    }
+
     return (
-        <div>UserPages</div>
+        <Container sx={{ mt: 10 }}>
+            <Box
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                }}
+            >
+            </Box>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell align="left">Id</StyledTableCell>
+                            <StyledTableCell align="left">Name</StyledTableCell>
+                            <StyledTableCell align="left">Username</StyledTableCell>
+                            <StyledTableCell align="left">Email</StyledTableCell>
+                            <StyledTableCell align="left">Company&nbsp;name</StyledTableCell>
+                            <StyledTableCell align="center">Actions</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {UserData.map((row) => (
+                            <StyledTableRow key={row.id}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.id}
+                                </StyledTableCell>
+                                <StyledTableCell align="left">{row.name}</StyledTableCell>
+                                <StyledTableCell align="left">{row.username}</StyledTableCell>
+                                <StyledTableCell align="left">{row.email}</StyledTableCell>
+                                <StyledTableCell align="left">{row.company.name}</StyledTableCell>
+                                <Button
+                                    sx={{ mt: 1 }}
+                                    variant="outlined"
+                                    disabled={pending}
+                                    onClick={() => onDeleteUser(row.id)}
+                                >
+                                    Delete
+                                </Button>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
     );
 }
-
-export default UserPages;
